77a9c06790d0a855c678d1cb6a7e060329b5d618
nesrinealoulou <nessrine.aloulou@enis.tn>
2024-09-11 10:05:27 +0100

    first commit

--- django-back-end/django_angular/settings.py
+++ django-back-end/django_angular/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'django_angular.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'enis_tp',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 3307,
     }
 }
 
